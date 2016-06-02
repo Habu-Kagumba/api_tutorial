87b8d3315acc56724c6492d017082851e9407694
Herbert Kagumba <habukagumba@gmail.com>
2016-06-02 16:36:22 +0300

    chore(benchmark): Improve requests/secs
    Remove the rails-api gem
    Introduce the oj json builder/parser
    Remove unncessary railties requirements
    Add custom ApiController with custom render method
    Add necessary middleware
    
    RESULT - Atleast 2000 req/sec (locally)

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "active_record/railtie"
+# require "action_view/railtie"
+require "action_mailer/railtie"
+# require "active_job/railtie"
+# require "action_cable/engine"
+require "rails/test_unit/railtie"
+# require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +29,11 @@ module ApiTutorial
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # remove unnecessary middleware
+    config.middleware.delete Rack::Sendfile
+    config.middleware.delete Rack::MethodOverride
+    config.middleware.delete ActionDispatch::Cookies
+    config.middleware.delete ActionDispatch::Session::CookieStore
+    config.middleware.delete ActionDispatch::Flash
   end
 end
